7a8525459fa9a3a8c457f5e8d78d5e9f7fd2eea4
Emme-Rita <atenuiemmerita@gmail.com>
2024-08-08 11:14:13 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+window.onresize = screen;
+window.onload = screen;
+
+
+function len() {
+    var a = 3;
+    console.log(a)
+}
+function screen(){
+    a = 2;
+    myWidth = window.innerWidth;
+
+    document.getElementById('size').innerHTML 
+    = "Width : ";
+}
